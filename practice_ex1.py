50f0d2459d1a248b9de6a736ac043c5ed2891f99
codewithhaykay <digitalmagnet506@gmail.com>
2025-07-24 10:09:41 +0100

    This is my first practice exercise on the zero to hero program.

---
+++ practice_ex1.py
@@ -0,0 +1,5 @@
+## This program ask the user for their name and favorite colors
+##and then print out their name and favorite color using f-string.
+your_name = input("Enter your name: ")
+fav_color = input("Enter your favourite colour: ")
+print(f'Hello {your_name}, your favourite colour is {fav_color}!')
