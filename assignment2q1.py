62bb4b4ab8b08f6c45c30d9328751a0811bc335e
codewithhaykay <digitalmagnet506@gmail.com>
2025-07-25 02:03:46 +0100

    practicing with stuffs on assignment 2

--- assignment2q1.py
+++ assignment2q1.py
@@ -1,18 +1,25 @@
-# incremental = range(1, 6)
-# decremental = range(4, 0, -1)
-for i in range(1, 2):
-    for k in range(1, 6):
-        print('*' * k)
-        # print(['*' * k for k in range(4, 0, -1)])
-    for k in range(4, 0, -1):
-        print('*' * k)
-            
+# # incremental = range(1, 6)
+# # decremental = range(4, 0, -1)
 # for i in range(1, 2):
+#     for k in range(1, 6):
+#         print('*' * k)
+#         # print(['*' * k for k in range(4, 0, -1)])
 #     for k in range(4, 0, -1):
 #         print('*' * k)
+            
+# # for i in range(1, 2):
+# #     for k in range(4, 0, -1):
+# #         print('*' * k)
         
     
-# for i in range(2, 13):
-#     for k in range(1, 13):
-#         print(f'{i} by {k} = ', k * i )
-        
+# # for i in range(2, 13):
+# #     for k in range(1, 13):
+# #         print(f'{i} by {k} = ', k * i )
+for i in range(9):
+    for k in range(6):
+        if (k == 0 and i < 1) or (i ==1 and k < 2) or (i == 2 and k < 3) or (i == 3 and k < 4) or (i == 4 and k < 5) or (i == 5 and k < 4) or (i == 6 and k < 3) or (i == 7 and k < 2) or (i == 8 and k < 1) or i== 0:
+            print('*', end=' ')
+            
+        else:
+            print(' ', end=' ') 
+    print()
